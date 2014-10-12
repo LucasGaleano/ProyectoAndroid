4ba22d2cd46094741f098a6a96996b550bb20b1a
IvanSzkrabko <ivanleonelszk@hotmail.com>
2014-10-12 20:46:22 -0300

    Version vieja con audio

---
+++ Assets/scripts/HombreMovement.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HombreMovement : MonoBehaviour {
+
+	public float velocidad;
+	bool facingright = true;
+	Animator anim ; //= new Animator ();
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+
+	void Update () 
+	{
+		grounded = Physics2D.OverlapCircle (groundCheck.position,groundRadius,whatIsGround);
+		anim.SetBool ("Ground",grounded);
+		anim.SetFloat ("velocidadVertical",rigidbody2D.velocity.y);
+
+		if(grounded && Input.GetKeyDown (KeyCode.Space))
+	    {
+			anim.SetBool ("Ground",false);
+			rigidbody2D.AddForce (new Vector2 (0, 700));
+
+		}
+
+
+
+
+		float horizontal = Input.GetAxis ("Horizontal");
+		//bool saltar = Input.GetButtonDown ("Jump");
+
+		velocidad = 10;
+		/*
+		if (saltar == true) 
+		{
+			rigidbody2D.AddForce (new Vector2 (0, 700));
+		}
+		*/
+		anim.SetFloat ("horizontal", Mathf.Abs (horizontal));
+
+		rigidbody2D.velocity = new Vector2(horizontal * velocidad , rigidbody2D.velocity.y);
+
+		if (horizontal > 0 && !facingright || horizontal < 0 && facingright )
+			Flip ();
+
+	}
+
+
+
+	void Flip()
+	{
+		facingright = !facingright;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+
+	}
+
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
+}
+
+
---
+++ Assets/scripts/kirbyMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kirbyMovement : MonoBehaviour {
+	
+	public float horizontal;
+	public float speed = 1;
+	public Vector2 velocidad;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+		velocidad = new Vector2(horizontal,vertical);
+		rigidbody2D.velocity = velocidad * speed;
+
+
+
+	
+	}
+}
